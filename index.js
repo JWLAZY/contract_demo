6bc1afd627a25a416bc2786d952841ebe32712f5
郑建文 <zhengjianwen@outlook.com>
2018-03-08 15:23:05 +0800

    web3.js 简单使用

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const Web3 = require('web3');
 const ganache_cli = require('ganache-cli');
-
 const web3 = new Web3(ganache_cli.provider());
 
-web3.eth.getAccounts().then(console.log);
-// console.log(web3.eth.get);
+
+// 获取当前节点所有账号
+web3.eth.getAccounts().then(accounts => {
+    console.log(accounts);
+    // 获取账户余额
+    web3.eth.getBalance(accounts[0]).then(result => {
+        console.log(web3.utils.fromWei(result, 'ether'));
+    })
+});
+
+web3.eth.getBlockNumber().then(number => {
+    console.log('block:' + number);
+})
+
